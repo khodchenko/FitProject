d451b43dda926c8b5ef3928e7f8e7829bdcfb592
khodchenko <khoch3nk0@gmail.com>
2021-12-13 14:42:39 +0100

    supportActionBar

--- app/src/main/java/com/example/fitproject/MainActivity.kt
+++ app/src/main/java/com/example/fitproject/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.example.fitproject
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.core.view.GravityCompat
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var toolbar : Toolbar
+    lateinit var drawerLayout : DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+
     }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // The action bar home/up action should open or close the drawer.
+        when (item.itemId) {
+            android.R.id.home -> {
+                drawerLayout.openDrawer(GravityCompat.START)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
 }
