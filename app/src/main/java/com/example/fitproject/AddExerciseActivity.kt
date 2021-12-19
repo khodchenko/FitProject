baee7a46161b7040deed6d59f7ba884146ac6918
khodchenko <khoch3nk0@gmail.com>
2021-12-19 21:58:05 +0100

    fix

--- app/src/main/java/com/example/fitproject/AddExerciseActivity.kt
+++ app/src/main/java/com/example/fitproject/AddExerciseActivity.kt
@@ -2,31 +2,39 @@ package com.example.fitproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.fitproject.adapters.RecyclerViewAdapter
 
+
 class AddExerciseActivity : AppCompatActivity() {
 
-    lateinit var exercisesList: ArrayList<Exercises>
+    private lateinit var recyclerAdapter: RecyclerViewAdapter
+    private lateinit var exercisesList: ArrayList<ExercisesModel>
+    private lateinit var recyclerview: RecyclerView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_new_exercise)
 
-        // Lookup the recyclerview in activity layout
-        val exercisesRecyclerView = findViewById<View>(R.id.recycler_view) as RecyclerView
+        init()
 
-        // Initialize exercises
-        exercisesList = Exercises.createEmptyList(20)
-        // Create adapter passing in the sample user data
-        val adapter = RecyclerViewAdapter(exercisesList)
-        // Attach the adapter to the recyclerview to populate items
-        exercisesRecyclerView.adapter = adapter
-        // Set layout manager to position the items
-        exercisesRecyclerView.layoutManager = LinearLayoutManager(this)
-        
 
+    }
+
+    private fun init(){
+        exercisesList = ArrayList()
+        exercisesList.add(ExercisesModel(R.drawable.ic_home, "TEST"))
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+        recyclerAdapter = RecyclerViewAdapter(this, exercisesList)
+        recyclerview = findViewById(R.id.recycler_view)
+        recyclerview.layoutManager = LinearLayoutManager(this)
+        recyclerview.adapter = recyclerAdapter
+        recyclerview.setHasFixedSize(true)
     }
 }
--- app/src/main/java/com/example/fitproject/adapters/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/fitproject/adapters/RecyclerViewAdapter.kt
@@ -1,36 +1,32 @@
 package com.example.fitproject.adapters
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.fitproject.Exercises
+import com.example.fitproject.ExercisesModel
 import com.example.fitproject.R
 
-class RecyclerViewAdapter (private val exercisesList: List<Exercises>): RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
+class RecyclerViewAdapter(var context: Context, var exercisesList: ArrayList<ExercisesModel>): RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
 
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val exercisesImage = itemView.findViewById<ImageView>(R.id.iv_exerciseItem)
-        val exercisesName = itemView.findViewById<TextView>(R.id.tv_exersiceItem)
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {}
 
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        // Create a new view, which defines the UI of the list item
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.add_exersice_item, viewGroup, false)
+        return ViewHolder(view)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val context = parent.context
-        val inflater = LayoutInflater.from(context)
-        val contactView = inflater.inflate(R.layout.add_exersice_item, parent, false)
-        return ViewHolder(contactView)
-    }
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        val item = exercisesList[position]
+        viewHolder.itemView.findViewById<TextView>(R.id.tv_exersiceItem).text = item.exerciseName
+        viewHolder.itemView.findViewById<ImageView>(R.id.iv_exerciseItem).setImageResource(item.exerciseImage)
 
-    override fun onBindViewHolder(viewHolder: RecyclerViewAdapter.ViewHolder, position: Int) {
-        val exercise: Exercises = exercisesList[position]
-        val exercisesImage = viewHolder.exercisesImage
-        exercisesImage.setImageResource(exercise.exerciseImage)
-        val exercisesName = viewHolder.exercisesName
-        exercisesName.text = exercise.exerciseName
     }
 
     override fun getItemCount(): Int {
